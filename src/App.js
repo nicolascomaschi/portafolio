a9639baea95f39ee4e7ae7ecfb7e55eb9c846011
Nicolas Comaschi <nicolascomaschi@gmail.com>
2019-02-17 17:56:25 -0300

    proyect reac.js satrt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div class="container-fluid">
+          <div class="row">
+            <div id="header" className="col-4 bg-dark">
+                <div className="col-12">
+                  <Navigation title="My Portfolio" />
+                </div>
+                <footer className="col-12">
+                  <ul class="list-group list-group-horizontal-lg">
+                    <li class="list-group-item">F</li>
+                    <li class="list-group-item">I</li>
+                    <li class="list-group-item">T</li>
+                    <li class="list-group-item">L</li>
+                  </ul>
+              </footer>
+            </div>
+            <div id="page" className="col-8">
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
